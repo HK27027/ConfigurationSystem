b013cc3b7185233be55a567109833f5d236198ea
huseyinkrs <121403058+HK27027@users.noreply.github.com>
2025-07-20 03:51:12 +0300

    .

--- Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
+++ Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eab8fe77d28a116d8d8de6ca28853c2997a9ada")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2771847d5e307c1619e859b90e7f5fd545887451")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
